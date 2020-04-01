fa94ece13b21e2cc269370062e0c0a40c3e0079a
birchtree <1980878786@qq.com>
2020-04-01 21:14:52 +0800

    增加随机生成long long范围内的数

--- random.h
+++ random.h
@@ -1,7 +1,15 @@
-#include <bits/stdc++.h>
+#include <cstdlib> //一下子把bit搞进去不怕重名吗 
 int rand_int(){
 	return rand()|(rand()<<15);
+}
+long long rand_long_long(){
+	long long x=rand_int(); 
+	return x|(x<<32ll); 
 }
 int random(int l,int r){
 	return rand_int()%(r-l+1);
 }
+long long random(long long l,long long r){
+	return rand_long_long()%(r-l+1);
+}
+
