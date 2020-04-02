60712dc8e316e8a05dd12479af0fd3b07d51f18f
birchtree <1980878786@qq.com>
2020-04-02 22:37:55 +0800

    增加图的自定义

--- test_gen.cpp
+++ test_gen.cpp
@@ -1,7 +1,10 @@
 #include"random.h"
 #include"gen_graph.h"
 #include"gen_seq.h"
+#include"gen_string.h"
+#include<ctime>
 using namespace std;
 int main(){
-	random_graph(5,8).print();
+	srand(time(NULL));
+	random_graph(5,10,CONNECT,ALLOW_SAME_EDGE,NO_SELF_LOOP).print();
 }
