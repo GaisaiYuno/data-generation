3e3a78014f3db1d320c5ebb7c0f1dad377be4fe7
GaisaiYuno <steven_mhy@outlook.com>
2020-04-03 14:30:00 +0800

    Update random.h

--- random.h
+++ random.h
@@ -3,12 +3,13 @@ int rand_int(){
 	return rand()|(rand()<<15);
 }
 long long rand_long_long(){
-	long long x=rand_int(); 
-	return x|(x<<32ll); 
+	long long x=rand_int();
+	long long y=rand_int();//这样才随机 
+	return x|(y<<32ll);
 }
 int random(int l,int r){
 	return l+rand_int()%(r-l+1);
 }
 long long random(long long l,long long r){
-	return r+rand_long_long()%(r-l+1);
+	return l+rand_long_long()%(r-l+1);
 }
